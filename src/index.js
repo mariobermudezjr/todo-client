9614e70ab5f98697a22a84bd51b73644b16c0558
Mario Bermudez Jr <mario.bermudez.jr@outlook.com>
2019-05-07 12:29:50 -0700

    [refactor] Add redux files and folders from redux todo examples TODO: Still need to stitch all the folders together

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
+import { render } from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import App from './components/App';
+import reducer from './reducers';
+
+const store = createStore(reducer);
+
+render(
+  <Provider store={store}>
+    <App />{' '}
+  </Provider>,
+  document.getElementById('root')
+);
